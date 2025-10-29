b42ba317b84e67d7edeef265f1a8c00e7e37d687
nican0r <94120714+nican0r@users.noreply.github.com>
2025-10-29 15:41:05 +0800

    feat: initial clean scaffolding

--- test/recon/CryticTester.sol
+++ test/recon/CryticTester.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: GPL-2.0
 pragma solidity ^0.8.0;
 
-import {TargetFunctions} from "./TargetFunctions.sol";
 import {CryticAsserts} from "@chimera/CryticAsserts.sol";
 
-// echidna . --contract CryticTester --config echidna.yaml
-// echidna . --contract CryticTester --config echidna.yaml --format text --test-limit 1000000 --test-mode assertion --workers 10
+import {TargetFunctions} from "./TargetFunctions.sol";
+
+// echidna . --contract CryticTester --config echidna.yaml --format text --workers 16 --test-limit 1000000
 // medusa fuzz
 contract CryticTester is TargetFunctions, CryticAsserts {
     constructor() payable {
