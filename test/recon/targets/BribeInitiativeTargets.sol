b4684b4c2d9286f8beac21b5ae420052d73ebaaa
nican0r <nican0r@nican0rs-Virtual-Machine.local>
2025-10-29 20:48:19 -0700

    feat: initial scaffolding

---
+++ test/recon/targets/BribeInitiativeTargets.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: GPL-2.0
+pragma solidity ^0.8.0;
+
+import {BaseTargetFunctions} from "@chimera/BaseTargetFunctions.sol";
+import {BeforeAfter} from "../BeforeAfter.sol";
+import {Properties} from "../Properties.sol";
+// Chimera deps
+import {vm} from "@chimera/Hevm.sol";
+
+// Helpers
+import {Panic} from "@recon/Panic.sol";
+
+import "src/BribeInitiative.sol";
+
+abstract contract BribeInitiativeTargets is
+    BaseTargetFunctions,
+    Properties
+{
+    /// CUSTOM TARGET FUNCTIONS - Add your own target functions here ///
+
+
+    /// AUTO GENERATED TARGET FUNCTIONS - WARNING: DO NOT DELETE OR MODIFY THIS LINE ///
+
+    function bribeInitiative_claimBribes(IBribeInitiative.ClaimData[] memory _claimData) public asActor {
+        bribeInitiative.claimBribes(_claimData);
+    }
+
+    function bribeInitiative_depositBribe(uint256 _boldAmount, uint256 _bribeTokenAmount, uint256 _epoch) public asActor {
+        bribeInitiative.depositBribe(_boldAmount, _bribeTokenAmount, _epoch);
+    }
+
+    function bribeInitiative_onAfterAllocateLQTY(uint256 _currentEpoch, address _user, IGovernance.UserState memory , IGovernance.Allocation memory _allocation, IGovernance.InitiativeState memory _initiativeState) public asActor {
+        bribeInitiative.onAfterAllocateLQTY(_currentEpoch, _user, "__HandleMe__", _allocation, _initiativeState);
+    }
+
+    function bribeInitiative_onClaimForInitiative(uint256 , uint256 ) public asActor {
+        bribeInitiative.onClaimForInitiative(0, 0);
+    }
+
+    function bribeInitiative_onRegisterInitiative(uint256 ) public asActor {
+        bribeInitiative.onRegisterInitiative(0);
+    }
+
+    function bribeInitiative_onUnregisterInitiative(uint256 ) public asActor {
+        bribeInitiative.onUnregisterInitiative(0);
+    }
+}
---
+++ test/recon/targets/GovernanceTargets.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: GPL-2.0
+pragma solidity ^0.8.0;
+
+import {BaseTargetFunctions} from "@chimera/BaseTargetFunctions.sol";
+import {BeforeAfter} from "../BeforeAfter.sol";
+import {Properties} from "../Properties.sol";
+// Chimera deps
+import {vm} from "@chimera/Hevm.sol";
+
+// Helpers
+import {Panic} from "@recon/Panic.sol";
+
+import "src/Governance.sol";
+
+abstract contract GovernanceTargets is
+    BaseTargetFunctions,
+    Properties
+{
+    /// CUSTOM TARGET FUNCTIONS - Add your own target functions here ///
+
+
+    /// AUTO GENERATED TARGET FUNCTIONS - WARNING: DO NOT DELETE OR MODIFY THIS LINE ///
+
+    function governance_allocateLQTY(address[] memory _initiativesToReset, address[] memory _initiatives, int256[] memory _absoluteLQTYVotes, int256[] memory _absoluteLQTYVetos) public asActor {
+        governance.allocateLQTY(_initiativesToReset, _initiatives, _absoluteLQTYVotes, _absoluteLQTYVetos);
+    }
+
+    function governance_calculateVotingThreshold() public asActor {
+        governance.calculateVotingThreshold();
+    }
+
+    function governance_claimForInitiative(address _initiative) public asActor {
+        governance.claimForInitiative(_initiative);
+    }
+
+    function governance_claimFromStakingV1(address _rewardRecipient) public asActor {
+        governance.claimFromStakingV1(_rewardRecipient);
+    }
+
+    function governance_deployUserProxy() public asActor {
+        governance.deployUserProxy();
+    }
+
+    function governance_depositLQTY(uint256 _lqtyAmount) public asActor {
+        governance.depositLQTY(_lqtyAmount);
+    }
+
+    function governance_depositLQTY(uint256 _lqtyAmount, bool _doSendRewards, address _recipient) public asActor {
+        governance.depositLQTY(_lqtyAmount, _doSendRewards, _recipient);
+    }
+
+    function governance_depositLQTYViaPermit(uint256 _lqtyAmount, PermitParams memory _permitParams, bool _doSendRewards, address _recipient) public asActor {
+        governance.depositLQTYViaPermit(_lqtyAmount, _permitParams, _doSendRewards, _recipient);
+    }
+
+    function governance_depositLQTYViaPermit(uint256 _lqtyAmount, PermitParams memory _permitParams) public asActor {
+        governance.depositLQTYViaPermit(_lqtyAmount, _permitParams);
+    }
+
+    function governance_getInitiativeState(address _initiative) public asActor {
+        governance.getInitiativeState(_initiative);
+    }
+
+    function governance_multiDelegateCall(bytes[] memory inputs) public asActor {
+        governance.multiDelegateCall(inputs);
+    }
+
+    function governance_registerInitialInitiatives(address[] memory _initiatives) public asActor {
+        governance.registerInitialInitiatives(_initiatives);
+    }
+
+    function governance_registerInitiative(address _initiative) public asActor {
+        governance.registerInitiative(_initiative);
+    }
+
+    function governance_resetAllocations(address[] memory _initiativesToReset, bool checkAll) public asActor {
+        governance.resetAllocations(_initiativesToReset, checkAll);
+    }
+
+    function governance_snapshotVotesForInitiative(address _initiative) public asActor {
+        governance.snapshotVotesForInitiative(_initiative);
+    }
+
+    function governance_unregisterInitiative(address _initiative) public asActor {
+        governance.unregisterInitiative(_initiative);
+    }
+
+    function governance_withdrawLQTY(uint256 _lqtyAmount) public asActor {
+        governance.withdrawLQTY(_lqtyAmount);
+    }
+
+    function governance_withdrawLQTY(uint256 _lqtyAmount, bool _doSendRewards, address _recipient) public asActor {
+        governance.withdrawLQTY(_lqtyAmount, _doSendRewards, _recipient);
+    }
+}
