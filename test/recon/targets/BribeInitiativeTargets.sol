7b1d6618740c35e46e7300549bb61e2ab0439c31
nican0r <94120714+nican0r@users.noreply.github.com>
2025-11-18 12:03:50 +0800

    chore: remove clamped handlers

--- test/recon/targets/BribeInitiativeTargets.sol
+++ test/recon/targets/BribeInitiativeTargets.sol
@@ -15,105 +15,19 @@ import "src/BribeInitiative.sol";
 abstract contract BribeInitiativeTargets is BaseTargetFunctions, Properties {
     /// CUSTOM TARGET FUNCTIONS - Add your own target functions here ///
 
-    function clamped_bribeInitiative_depositBribe(uint256 _boldAmount, uint256 _bribeTokenAmount, uint256 _epoch)
-        public
-        asActor
-    {
-        // Apply meaningful values from meaningful-values.json
-        _epoch = governance.epoch();
-
-        // Clamp amounts using modulo arithmetic
-        _boldAmount %= (bold.balanceOf(_getActor()) + 1);
-        _bribeTokenAmount %= (bribeToken.balanceOf(_getActor()) + 1);
-
-        bribeInitiative_depositBribe(_boldAmount, _bribeTokenAmount, _epoch);
-    }
-
-    function clamped_bribeInitiative_claimBribes(IBribeInitiative.ClaimData[] memory _claimData) public asActor {
-        // Apply meaningful values from meaningful-values.json
-        _claimData = new IBribeInitiative.ClaimData[](1);
-        _claimData[0] = IBribeInitiative.ClaimData({
-            epoch: governance.epoch(),
-            prevLQTYAllocationEpoch: 0,
-            prevTotalLQTYAllocationEpoch: 0
-        });
-
-        bribeInitiative_claimBribes(_claimData);
-    }
-
-    function clamped_bribeInitiative_onAfterAllocateLQTY(
-        uint256 _currentEpoch,
-        address _user,
-        IGovernance.UserState memory _userState,
-        IGovernance.Allocation memory _allocation,
-        IGovernance.InitiativeState memory _initiativeState
-    ) public asActor {
-        // Apply meaningful values from meaningful-values.json
-        _currentEpoch = governance.epoch();
-        _user = _getActor();
-        (uint256 unallocatedLQTY, uint256 unallocatedOffset, uint256 allocatedLQTY, uint256 allocatedOffset) =
-            governance.userStates(_user);
-        _userState = IGovernance.UserState({
-            unallocatedLQTY: unallocatedLQTY,
-            unallocatedOffset: unallocatedOffset,
-            allocatedLQTY: allocatedLQTY,
-            allocatedOffset: allocatedOffset
-        });
-        _allocation = IGovernance.Allocation({
-            voteLQTY: uint256(1e18) % (unallocatedLQTY + 1),
-            voteOffset: 0,
-            vetoLQTY: uint256(1e18) % (unallocatedLQTY + 1),
-            vetoOffset: 0,
-            atEpoch: _currentEpoch
-        });
-        (uint256 voteLQTY, uint256 voteOffset, uint256 vetoLQTY, uint256 vetoOffset, uint256 lastEpochClaim) =
-            governance.initiativeStates(address(bribeInitiative));
-        _initiativeState = IGovernance.InitiativeState({
-            voteLQTY: voteLQTY,
-            voteOffset: voteOffset,
-            vetoLQTY: vetoLQTY,
-            vetoOffset: vetoOffset,
-            lastEpochClaim: lastEpochClaim
-        });
-
-        bribeInitiative_onAfterAllocateLQTY(_currentEpoch, _user, _userState, _allocation, _initiativeState);
-    }
-
-    function clamped_bribeInitiative_onClaimForInitiative(uint256 _currentEpoch, uint256 _rewardAmount)
-        public
-        asActor
-    {
-        // Apply meaningful values from meaningful-values.json
-        _currentEpoch = governance.epoch();
-        _rewardAmount = 1000e18; // minAccrual value from configuration
-
-        bribeInitiative_onClaimForInitiative(_currentEpoch, _rewardAmount);
-    }
-
-    function clamped_bribeInitiative_onRegisterInitiative(uint256 _currentEpoch) public asActor {
-        // Apply meaningful values from meaningful-values.json
-        _currentEpoch = governance.epoch();
-
-        bribeInitiative_onRegisterInitiative(_currentEpoch);
-    }
-
-    function clamped_bribeInitiative_onUnregisterInitiative(uint256 _currentEpoch) public asActor {
-        // Apply meaningful values from meaningful-values.json
-        _currentEpoch = governance.epoch();
-
-        bribeInitiative_onUnregisterInitiative(_currentEpoch);
-    }
-
     /// AUTO GENERATED TARGET FUNCTIONS - WARNING: DO NOT DELETE OR MODIFY THIS LINE ///
 
-    function bribeInitiative_claimBribes(IBribeInitiative.ClaimData[] memory _claimData) public asActor {
+    function bribeInitiative_claimBribes(
+        IBribeInitiative.ClaimData[] memory _claimData
+    ) public asActor {
         bribeInitiative.claimBribes(_claimData);
     }
 
-    function bribeInitiative_depositBribe(uint256 _boldAmount, uint256 _bribeTokenAmount, uint256 _epoch)
-        public
-        asActor
-    {
+    function bribeInitiative_depositBribe(
+        uint256 _boldAmount,
+        uint256 _bribeTokenAmount,
+        uint256 _epoch
+    ) public asActor {
         bribeInitiative.depositBribe(_boldAmount, _bribeTokenAmount, _epoch);
     }
 
@@ -124,10 +38,19 @@ abstract contract BribeInitiativeTargets is BaseTargetFunctions, Properties {
         IGovernance.Allocation memory _allocation,
         IGovernance.InitiativeState memory _initiativeState
     ) public asActor {
-        bribeInitiative.onAfterAllocateLQTY(_currentEpoch, _user, _userState, _allocation, _initiativeState);
+        bribeInitiative.onAfterAllocateLQTY(
+            _currentEpoch,
+            _user,
+            _userState,
+            _allocation,
+            _initiativeState
+        );
     }
 
-    function bribeInitiative_onClaimForInitiative(uint256, uint256) public asActor {
+    function bribeInitiative_onClaimForInitiative(
+        uint256,
+        uint256
+    ) public asActor {
         bribeInitiative.onClaimForInitiative(0, 0);
     }
 
--- test/recon/targets/GovernanceTargets.sol
+++ test/recon/targets/GovernanceTargets.sol
@@ -15,166 +15,6 @@ import "src/Governance.sol";
 abstract contract GovernanceTargets is BaseTargetFunctions, Properties {
     /// CUSTOM TARGET FUNCTIONS - Add your own target functions here ///
 
-    function clamped_governance_allocateLQTY(
-        address[] memory _initiativesToReset,
-        address[] memory _initiatives,
-        int256[] memory _absoluteLQTYVotes,
-        int256[] memory _absoluteLQTYVetos
-    ) public asActor {
-        // Apply meaningful values from meaningful-values.json
-        _initiativesToReset = new address[](0);
-        _initiatives = new address[](1);
-        _initiatives[0] = address(bribeInitiative);
-
-        // Clamp amounts using modulo arithmetic
-        (, uint256 unallocatedLQTY,,) = governance.userStates(_getActor());
-        for (uint i = 0; i < _absoluteLQTYVotes.length; i++) {
-            _absoluteLQTYVotes[i] = int256(uint256(_absoluteLQTYVotes[i]) % (unallocatedLQTY + 1));
-        }
-        for (uint i = 0; i < _absoluteLQTYVetos.length; i++) {
-            _absoluteLQTYVetos[i] = int256(uint256(_absoluteLQTYVetos[i]) % (unallocatedLQTY + 1));
-        }
-
-        governance_allocateLQTY(_initiativesToReset, _initiatives, _absoluteLQTYVotes, _absoluteLQTYVetos);
-    }
-
-    function clamped_governance_depositLQTY(uint256 _lqtyAmount, bool _doSendRewards, address _recipient)
-        public
-        asActor
-    {
-        // Apply meaningful values from meaningful-values.json
-        _doSendRewards = false;
-        _recipient = _getActor();
-
-        // Clamp amount using modulo arithmetic
-        _lqtyAmount %= (lqty.balanceOf(_getActor()) + 1);
-
-        governance_depositLQTY(_lqtyAmount, _doSendRewards, _recipient);
-    }
-
-    function clamped_governance_depositLQTYViaPermit(
-        uint256 _lqtyAmount,
-        PermitParams memory _permitParams,
-        bool _doSendRewards,
-        address _recipient
-    ) public asActor {
-        // Apply meaningful values from meaningful-values.json
-        _doSendRewards = false;
-        _recipient = _getActor();
-        _permitParams = PermitParams({
-            owner: _getActor(),
-            spender: address(governance),
-            value: 1e18,
-            deadline: block.timestamp + 1 days,
-            v: 27,
-            r: bytes32(uint256(1)),
-            s: bytes32(uint256(1))
-        });
-
-        // Clamp amount using modulo arithmetic
-        _lqtyAmount %= (lqty.balanceOf(_getActor()) + 1);
-        _permitParams.value = _lqtyAmount;
-
-        governance_depositLQTYViaPermit(_lqtyAmount, _permitParams, _doSendRewards, _recipient);
-    }
-
-    function clamped_governance_registerInitiative(address _initiative) public asActor {
-        // Apply meaningful values from meaningful-values.json
-        _initiative = address(bribeInitiative);
-
-        governance_registerInitiative(_initiative);
-    }
-
-    function clamped_governance_withdrawLQTY(uint256 _lqtyAmount, bool _doSendRewards, address _recipient)
-        public
-        asActor
-    {
-        // Apply meaningful values from meaningful-values.json
-        _doSendRewards = false;
-        _recipient = _getActor();
-
-        // Clamp amount using modulo arithmetic
-        (, uint256 unallocatedLQTY,,) = governance.userStates(_getActor());
-        _lqtyAmount %= (unallocatedLQTY + 1);
-
-        governance_withdrawLQTY(_lqtyAmount, _doSendRewards, _recipient);
-    }
-
-    function clamped_governance_resetAllocations(address[] memory _initiativesToReset, bool checkAll) public asActor {
-        // Apply meaningful values from meaningful-values.json
-        _initiativesToReset = new address[](0);
-        checkAll = false;
-
-        governance_resetAllocations(_initiativesToReset, checkAll);
-    }
-
-    function clamped_governance_multiDelegateCall(bytes[] memory inputs) public asActor {
-        // Apply meaningful values from meaningful-values.json
-        inputs = new bytes[](0);
-
-        governance_multiDelegateCall(inputs);
-    }
-
-    function clamped_governance_claimFromStakingV1(address _rewardRecipient) public asActor {
-        // Apply meaningful values from meaningful-values.json
-        _rewardRecipient = _getActor();
-
-        governance_claimFromStakingV1(_rewardRecipient);
-    }
-
-    function clamped_governance_calculateVotingThreshold() public asActor {
-        // No parameters to clamp, just call the function
-        governance_calculateVotingThreshold();
-    }
-
-    function clamped_governance_claimForInitiative(address _initiative) public asActor {
-        // Apply meaningful values from meaningful-values.json
-        _initiative = address(bribeInitiative);
-
-        governance_claimForInitiative(_initiative);
-    }
-
-    function clamped_governance_deployUserProxy() public asActor {
-        // No parameters to clamp, just call the function
-        governance_deployUserProxy();
-    }
-
-    function clamped_governance_depositLQTY_single(uint256 _lqtyAmount) public asActor {
-        // Apply meaningful values from meaningful-values.json
-        _lqtyAmount %= (lqty.balanceOf(_getActor()) + 1);
-
-        governance_depositLQTY(_lqtyAmount);
-    }
-
-    function clamped_governance_getInitiativeState(address _initiative) public asActor {
-        // Apply meaningful values from meaningful-values.json
-        _initiative = address(bribeInitiative);
-
-        governance_getInitiativeState(_initiative);
-    }
-
-    function clamped_governance_snapshotVotesForInitiative(address _initiative) public asActor {
-        // Apply meaningful values from meaningful-values.json
-        _initiative = address(bribeInitiative);
-
-        governance_snapshotVotesForInitiative(_initiative);
-    }
-
-    function clamped_governance_unregisterInitiative(address _initiative) public asActor {
-        // Apply meaningful values from meaningful-values.json
-        _initiative = address(bribeInitiative);
-
-        governance_unregisterInitiative(_initiative);
-    }
-
-    function clamped_governance_withdrawLQTY_single(uint256 _lqtyAmount) public asActor {
-        // Apply meaningful values from meaningful-values.json
-        (, uint256 unallocatedLQTY,,) = governance.userStates(_getActor());
-        _lqtyAmount %= (unallocatedLQTY + 1);
-
-        governance_withdrawLQTY(_lqtyAmount);
-    }
-
     /// AUTO GENERATED TARGET FUNCTIONS - WARNING: DO NOT DELETE OR MODIFY THIS LINE ///
 
     function governance_allocateLQTY(
@@ -183,7 +23,12 @@ abstract contract GovernanceTargets is BaseTargetFunctions, Properties {
         int256[] memory _absoluteLQTYVotes,
         int256[] memory _absoluteLQTYVetos
     ) public asActor {
-        governance.allocateLQTY(_initiativesToReset, _initiatives, _absoluteLQTYVotes, _absoluteLQTYVetos);
+        governance.allocateLQTY(
+            _initiativesToReset,
+            _initiatives,
+            _absoluteLQTYVotes,
+            _absoluteLQTYVetos
+        );
     }
 
     function governance_calculateVotingThreshold() public asActor {
@@ -194,7 +39,9 @@ abstract contract GovernanceTargets is BaseTargetFunctions, Properties {
         governance.claimForInitiative(_initiative);
     }
 
-    function governance_claimFromStakingV1(address _rewardRecipient) public asActor {
+    function governance_claimFromStakingV1(
+        address _rewardRecipient
+    ) public asActor {
         governance.claimFromStakingV1(_rewardRecipient);
     }
 
@@ -206,7 +53,11 @@ abstract contract GovernanceTargets is BaseTargetFunctions, Properties {
         governance.depositLQTY(_lqtyAmount);
     }
 
-    function governance_depositLQTY(uint256 _lqtyAmount, bool _doSendRewards, address _recipient) public asActor {
+    function governance_depositLQTY(
+        uint256 _lqtyAmount,
+        bool _doSendRewards,
+        address _recipient
+    ) public asActor {
         governance.depositLQTY(_lqtyAmount, _doSendRewards, _recipient);
     }
 
@@ -216,10 +67,18 @@ abstract contract GovernanceTargets is BaseTargetFunctions, Properties {
         bool _doSendRewards,
         address _recipient
     ) public asActor {
-        governance.depositLQTYViaPermit(_lqtyAmount, _permitParams, _doSendRewards, _recipient);
+        governance.depositLQTYViaPermit(
+            _lqtyAmount,
+            _permitParams,
+            _doSendRewards,
+            _recipient
+        );
     }
 
-    function governance_depositLQTYViaPermit(uint256 _lqtyAmount, PermitParams memory _permitParams) public asActor {
+    function governance_depositLQTYViaPermit(
+        uint256 _lqtyAmount,
+        PermitParams memory _permitParams
+    ) public asActor {
         governance.depositLQTYViaPermit(_lqtyAmount, _permitParams);
     }
 
@@ -227,7 +86,9 @@ abstract contract GovernanceTargets is BaseTargetFunctions, Properties {
         governance.getInitiativeState(_initiative);
     }
 
-    function governance_multiDelegateCall(bytes[] memory inputs) public asActor {
+    function governance_multiDelegateCall(
+        bytes[] memory inputs
+    ) public asActor {
         governance.multiDelegateCall(inputs);
     }
 
@@ -235,15 +96,22 @@ abstract contract GovernanceTargets is BaseTargetFunctions, Properties {
         governance.registerInitiative(_initiative);
     }
 
-    function governance_resetAllocations(address[] memory _initiativesToReset, bool checkAll) public asActor {
+    function governance_resetAllocations(
+        address[] memory _initiativesToReset,
+        bool checkAll
+    ) public asActor {
         governance.resetAllocations(_initiativesToReset, checkAll);
     }
 
-    function governance_snapshotVotesForInitiative(address _initiative) public asActor {
+    function governance_snapshotVotesForInitiative(
+        address _initiative
+    ) public asActor {
         governance.snapshotVotesForInitiative(_initiative);
     }
 
-    function governance_unregisterInitiative(address _initiative) public asActor {
+    function governance_unregisterInitiative(
+        address _initiative
+    ) public asActor {
         governance.unregisterInitiative(_initiative);
     }
 
@@ -251,7 +119,11 @@ abstract contract GovernanceTargets is BaseTargetFunctions, Properties {
         governance.withdrawLQTY(_lqtyAmount);
     }
 
-    function governance_withdrawLQTY(uint256 _lqtyAmount, bool _doSendRewards, address _recipient) public asActor {
+    function governance_withdrawLQTY(
+        uint256 _lqtyAmount,
+        bool _doSendRewards,
+        address _recipient
+    ) public asActor {
         governance.withdrawLQTY(_lqtyAmount, _doSendRewards, _recipient);
     }
 }
