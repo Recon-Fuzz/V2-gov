f7dafdf1b100821bb91ac9543d1e5163d7c94e43
nican0r <94120714+nican0r@users.noreply.github.com>
2025-11-20 11:06:15 +0800

    Step 4: Phase 3: Creating Clamped Handlers
    Implement clamped function handlers to achieve full coverage on the system setup

--- test/recon/targets/BribeInitiativeTargets.sol
+++ test/recon/targets/BribeInitiativeTargets.sol
@@ -16,6 +16,62 @@ import "src/BribeInitiative.sol";
 abstract contract BribeInitiativeTargets is BaseTargetFunctions, Properties {
     /// CUSTOM TARGET FUNCTIONS - Add your own target functions here //
 
+    function bribeInitiative_claimBribes_clamped() public asActor {
+        // Create a simple claim for current epoch - 1 (previous epoch)
+        uint256 currentEpoch = governance.epoch();
+        if (currentEpoch == 0) return;
+        
+        uint256 claimEpoch = currentEpoch - 1;
+        IBribeInitiative.ClaimData[] memory claimData = new IBribeInitiative.ClaimData[](1);
+        claimData[0] = IBribeInitiative.ClaimData({
+            epoch: claimEpoch,
+            prevLQTYAllocationEpoch: 0,
+            prevTotalLQTYAllocationEpoch: 0
+        });
+        
+        bribeInitiative_claimBribes(claimData);
+    }
+
+    function bribeInitiative_depositBribe_clamped(uint256 _boldAmount, uint256 _bribeTokenAmount) public asActor {
+        // Clamp amounts to reasonable values that won't revert
+        uint256 currentEpoch = governance.epoch();
+        address actor = _getActor();
+        
+        // Use actor's token balances as upper bounds
+        uint256 maxBold = bold.balanceOf(actor);
+        uint256 maxBribeToken = bribeToken.balanceOf(actor);
+        
+        // Ensure at least 1 token is deposited if balance > 0
+        if (maxBold > 0) {
+            _boldAmount = (_boldAmount % maxBold) + 1;
+        } else {
+            _boldAmount = 0;
+        }
+        
+        if (maxBribeToken > 0) {
+            _bribeTokenAmount = (_bribeTokenAmount % maxBribeToken) + 1;
+        } else {
+            _bribeTokenAmount = 0;
+        }
+        
+        bribeInitiative_depositBribe(_boldAmount, _bribeTokenAmount, currentEpoch);
+    }
+
+    function bribeInitiative_depositBribe_currentEpoch_clamped() public asActor {
+        // Deposit bribe for current epoch with reasonable amounts
+        address actor = _getActor();
+        uint256 currentEpoch = governance.epoch();
+        
+        uint256 boldBalance = bold.balanceOf(actor);
+        uint256 bribeTokenBalance = bribeToken.balanceOf(actor);
+        
+        // Use 10% of balance as a reasonable bribe amount
+        uint256 boldAmount = boldBalance > 0 ? boldBalance / 10 : 0;
+        uint256 bribeTokenAmount = bribeTokenBalance > 0 ? bribeTokenBalance / 10 : 0;
+        
+        bribeInitiative_depositBribe(boldAmount, bribeTokenAmount, currentEpoch);
+    }
+
     /// AUTO GENERATED TARGET FUNCTIONS - WARNING: DO NOT DELETE OR MODIFY THIS LINE ///
 
     function bribeInitiative_claimBribes(
--- test/recon/targets/GovernanceTargets.sol
+++ test/recon/targets/GovernanceTargets.sol
@@ -17,6 +17,97 @@ import "src/Governance.sol";
 abstract contract GovernanceTargets is BaseTargetFunctions, Properties {
     /// CUSTOM TARGET FUNCTIONS - Add your own target functions here ///
 
+    function governance_allocateLQTY_clamped(uint256 _voteAmount, uint256 _vetoAmount) public asActor {
+        address actor = _getActor();
+        
+        // Get current user state to determine available LQTY
+        IGovernance.UserState memory userState = governance.userStates(actor);
+        uint256 availableLQTY = userState.lqtyDeposited;
+        
+        // Clamp vote and veto amounts to available LQTY
+        if (availableLQTY > 0) {
+            _voteAmount = _voteAmount % (availableLQTY + 1);
+            _vetoAmount = _vetoAmount % (availableLQTY + 1);
+        } else {
+            _voteAmount = 0;
+            _vetoAmount = 0;
+        }
+        
+        // Create allocation arrays with bribe initiative
+        address[] memory initiatives = new address[](1);
+        initiatives[0] = address(bribeInitiative);
+        
+        int256[] memory votes = new int256[](1);
+        votes[0] = int256(_voteAmount);
+        
+        int256[] memory vetos = new int256[](1);
+        vetos[0] = int256(_vetoAmount);
+        
+        address[] memory emptyArray = new address[](0);
+        
+        governance_allocateLQTY(emptyArray, initiatives, votes, vetos);
+    }
+
+    function governance_depositLQTY_clamped(uint256 _lqtyAmount) public asActor {
+        address actor = _getActor();
+        uint256 maxBalance = lqty.balanceOf(actor);
+        
+        // Clamp to actor's balance, allowing full balance deposit
+        if (maxBalance > 0) {
+            _lqtyAmount = (_lqtyAmount % (maxBalance + 1));
+        } else {
+            _lqtyAmount = 0;
+        }
+        
+        governance_depositLQTY(_lqtyAmount);
+    }
+
+    function governance_depositLQTY_max_clamped() public asActor {
+        // Deposit maximum possible amount
+        address actor = _getActor();
+        uint256 balance = lqty.balanceOf(actor);
+        
+        governance_depositLQTY(balance);
+    }
+
+    function governance_withdrawLQTY_clamped(uint256 _lqtyAmount) public asActor {
+        address actor = _getActor();
+        IGovernance.UserState memory userState = governance.userStates(actor);
+        uint256 deposited = userState.lqtyDeposited;
+        
+        // Clamp to deposited amount
+        if (deposited > 0) {
+            _lqtyAmount = (_lqtyAmount % (deposited + 1));
+        } else {
+            _lqtyAmount = 0;
+        }
+        
+        governance_withdrawLQTY(_lqtyAmount);
+    }
+
+    function governance_withdrawLQTY_all_clamped() public asActor {
+        // Withdraw all deposited LQTY
+        address actor = _getActor();
+        IGovernance.UserState memory userState = governance.userStates(actor);
+        
+        governance_withdrawLQTY(userState.lqtyDeposited);
+    }
+
+    function governance_registerInitiative_clamped() public asActor {
+        // Register the bribe initiative
+        governance_registerInitiative(address(bribeInitiative));
+    }
+
+    function governance_claimForInitiative_clamped() public asActor {
+        // Claim for bribe initiative
+        governance_claimForInitiative(address(bribeInitiative));
+    }
+
+    function governance_unregisterInitiative_clamped() public asActor {
+        // Unregister the bribe initiative
+        governance_unregisterInitiative(address(bribeInitiative));
+    }
+
     /// AUTO GENERATED TARGET FUNCTIONS - WARNING: DO NOT DELETE OR MODIFY THIS LINE ///
 
     function governance_allocateLQTY(
