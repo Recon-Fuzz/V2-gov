ffc92f5f5a5ab293fba160c40d1d75de31aba457
nican0r <94120714+nican0r@users.noreply.github.com>
2025-11-21 14:27:54 +0800

    Step 1: Phase 0: Identifying Contracts to Cover
    Identify all contracts in the current repository that must always be fully covered by the fuzzer

--- test/recon/targets/AdminTargets.sol
+++ test/recon/targets/AdminTargets.sol
@@ -12,47 +12,5 @@ import {Panic} from "@recon/Panic.sol";
 
 abstract contract AdminTargets is BaseTargetFunctions, Properties {
     /// CUSTOM TARGET FUNCTIONS - Add your own target functions here ///
-
-    function governance_registerInitialInitiatives_clamped(uint256 _numInitiatives) public asAdmin {
-        // Limit number of initiatives to reasonable range
-        uint256 numInitiatives = (_numInitiatives % 3) + 1; // 1-3 initiatives
-        address[] memory initiatives = new address[](numInitiatives);
-        
-        // Use bribe initiative and actors as meaningful values
-        initiatives[0] = address(bribeInitiative);
-        address[] memory actors = _getActors();
-        for (uint256 i = 1; i < numInitiatives; i++) {
-            initiatives[i] = actors[i % actors.length];
-        }
-        
-        governance_registerInitialInitiatives(initiatives);
-    }
-
-    function governance_multiDelegateCall_clamped(uint256 _numCalls) public asAdmin {
-        // Limit number of delegate calls to reasonable range
-        uint256 numCalls = (_numCalls % 3) + 1; // 1-3 calls
-        bytes[] memory calls = new bytes[](numCalls);
-        
-        // Create simple delegate calls for common functions
-        for (uint256 i = 0; i < numCalls; i++) {
-            // Simple call to calculateVotingThreshold (no parameters needed)
-            calls[i] = abi.encodeWithSignature("calculateVotingThreshold()");
-        }
-        
-        governance_multiDelegateCall(calls);
-    }
-
-    function governance_registerInitialInitiatives(
-        address[] memory _initiatives
-    ) public asAdmin {
-        governance.registerInitialInitiatives(_initiatives);
-    }
-
-    function governance_multiDelegateCall(
-        bytes[] memory inputs
-    ) public asAdmin {
-        governance.multiDelegateCall(inputs);
-    }
-
     /// AUTO GENERATED TARGET FUNCTIONS - WARNING: DO NOT DELETE OR MODIFY THIS LINE ///
 }
