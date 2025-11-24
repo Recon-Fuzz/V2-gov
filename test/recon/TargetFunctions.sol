ecdd613173a12c5f136e29b1f76d023a3246a9f9
nican0r <94120714+nican0r@users.noreply.github.com>
2025-11-24 17:05:47 +0800

    Step 5: Phase 4: Creating Shortcut Handlers
    Implement shortcut handlers that allow the fuzzer to reach coverage faster.

--- test/recon/TargetFunctions.sol
+++ test/recon/TargetFunctions.sol
@@ -33,5 +33,93 @@ abstract contract TargetFunctions is
     // UniV4MerklRewardsTargets
 {
     /// CUSTOM TARGET FUNCTIONS - Add your own target functions here ///
+
+    function shortcut_claimForInitiative(uint256 _lqtyAmount, uint256 _voteAmount) public {
+        // First register the initiative
+        governance_registerInitiative_clamped();
+        
+        // Deposit LQTY to have unallocated LQTY
+        governance_depositLQTY_clamped(_lqtyAmount);
+        
+        // Allocate LQTY to bribe initiative
+        address[] memory initiativesToReset = new address[](0);
+        address[] memory initiatives = new address[](1);
+        int256[] memory votes = new int256[](1);
+        int256[] memory vetos = new int256[](1);
+        
+        initiatives[0] = address(bribeInitiative);
+        votes[0] = int256(_voteAmount);
+        vetos[0] = int256(0);
+        
+        governance_allocateLQTY_clamped(initiativesToReset, initiatives, votes, vetos);
+        
+        // Fast forward time to next epoch to make claimable
+        vm.warp(block.timestamp + 604800); // 1 week (epoch duration)
+        
+        // Claim for the initiative
+        governance_claimForInitiative_clamped();
+    }
+
+    function shortcut_claimBribes(uint256 _lqtyAmount, uint256 _voteAmount, uint256 _boldAmount, uint256 _bribeTokenAmount) public {
+        // Deposit LQTY to have unallocated LQTY
+        governance_depositLQTY_clamped(_lqtyAmount);
+        
+        // Allocate LQTY to bribe initiative
+        address[] memory initiativesToReset = new address[](0);
+        address[] memory initiatives = new address[](1);
+        int256[] memory votes = new int256[](1);
+        int256[] memory vetos = new int256[](1);
+        
+        initiatives[0] = address(bribeInitiative);
+        votes[0] = int256(_voteAmount);
+        vetos[0] = int256(0);
+        
+        governance_allocateLQTY_clamped(initiativesToReset, initiatives, votes, vetos);
+        
+        // Deposit bribe for current epoch
+        uint256 currentEpoch = governance.epoch();
+        bribeInitiative_depositBribe_clamped(_boldAmount, _bribeTokenAmount, currentEpoch);
+        
+        // Fast forward time to next epoch to enable claiming
+        vm.warp(block.timestamp + 604800); // 1 week (epoch duration)
+        
+        // Claim bribes
+        bribeInitiative_claimBribes_clamped();
+    }
+
+    function shortcut_fullVotingCycle(uint256 _lqtyAmount, uint256 _voteAmount, uint256 _boldAmount, uint256 _bribeTokenAmount) public {
+        // Complete voting cycle: register -> allocate -> claim rewards -> claim bribes
+        
+        // 1. Deposit LQTY to have voting power
+        governance_depositLQTY_clamped(_lqtyAmount);
+        
+        // 2. Register initiative
+        governance_registerInitiative_clamped();
+        
+        // 3. Allocate LQTY to initiative
+        address[] memory initiativesToReset = new address[](0);
+        address[] memory initiatives = new address[](1);
+        int256[] memory votes = new int256[](1);
+        int256[] memory vetos = new int256[](1);
+        
+        initiatives[0] = address(bribeInitiative);
+        votes[0] = int256(_voteAmount);
+        vetos[0] = int256(0);
+        
+        governance_allocateLQTY_clamped(initiativesToReset, initiatives, votes, vetos);
+        
+        // 4. Deposit bribe
+        uint256 currentEpoch = governance.epoch();
+        bribeInitiative_depositBribe_clamped(_boldAmount, _bribeTokenAmount, currentEpoch);
+        
+        // 5. Fast forward to next epoch
+        vm.warp(block.timestamp + 604800); // 1 week
+        
+        // 6. Claim initiative rewards
+        governance_claimForInitiative_clamped();
+        
+        // 7. Claim bribes
+        bribeInitiative_claimBribes_clamped();
+    }
     /// AUTO GENERATED TARGET FUNCTIONS - WARNING: DO NOT DELETE OR MODIFY THIS LINE ///
 }
