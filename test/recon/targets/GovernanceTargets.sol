bbcc8811c23474d1b0a136698b2a2dd7e11aaaae
nican0r <94120714+nican0r@users.noreply.github.com>
2025-11-24 16:50:37 +0800

    chore: remove artifacts from latest run

--- test/recon/targets/GovernanceTargets.sol
+++ test/recon/targets/GovernanceTargets.sol
@@ -17,190 +17,6 @@ import "src/Governance.sol";
 abstract contract GovernanceTargets is BaseTargetFunctions, Properties {
     /// CUSTOM TARGET FUNCTIONS - Add your own target functions here ///
 
-    function governance_allocateLQTY_clamped(
-        uint256 _randomVotes,
-        uint256 _randomVetos
-    ) public asActor {
-        (uint256 unallocatedLQTY,,,) = governance.userStates(_getActor());
-        
-        address[] memory initiativesToReset = new address[](0);
-        address[] memory initiatives = new address[](1);
-        initiatives[0] = address(bribeInitiative);
-        
-        int256[] memory absoluteLQTYVotes = new int256[](1);
-        int256[] memory absoluteLQTYVetos = new int256[](1);
-        
-        absoluteLQTYVotes[0] = int256(_randomVotes % (unallocatedLQTY + 1));
-        absoluteLQTYVetos[0] = int256(_randomVetos % (unallocatedLQTY + 1));
-        
-        governance_allocateLQTY(initiativesToReset, initiatives, absoluteLQTYVotes, absoluteLQTYVetos);
-    }
-
-    function governance_allocateLQTYLateEpoch_clamped(uint256 _randomVotes, uint256 _randomVetos) public asActor {
-        // Fast forward to near end of epoch to trigger late allocation logic
-        uint256 currentEpoch = governance.epoch();
-        uint256 epochVotingCutoff = governance.EPOCH_VOTING_CUTOFF();
-        uint256 epochStart = governance.EPOCH_START();
-        uint256 epochDuration = governance.EPOCH_DURATION();
-        
-        uint256 currentEpochStart = epochStart + (currentEpoch * epochDuration);
-        uint256 votingCutoffTime = currentEpochStart + epochVotingCutoff;
-        
-        // Only warp if we're before the voting cutoff
-        if (block.timestamp < votingCutoffTime) {
-            vm.warp(votingCutoffTime + 100);
-        }
-        
-        (uint256 unallocatedLQTY,,,) = governance.userStates(_getActor());
-        
-        address[] memory initiativesToReset = new address[](0);
-        address[] memory initiatives = new address[](1);
-        initiatives[0] = address(bribeInitiative);
-        
-        int256[] memory absoluteLQTYVotes = new int256[](1);
-        int256[] memory absoluteLQTYVetos = new int256[](1);
-        
-        absoluteLQTYVotes[0] = int256(_randomVotes % (unallocatedLQTY + 1));
-        absoluteLQTYVetos[0] = int256(_randomVetos % (unallocatedLQTY + 1));
-        
-        governance_allocateLQTY(initiativesToReset, initiatives, absoluteLQTYVotes, absoluteLQTYVetos);
-    }
-
-    function governance_depositLQTY_clamped(uint256 _lqtyAmount) public asActor {
-        _lqtyAmount %= lqty.balanceOf(_getActor()) + 1;
-        
-        governance_depositLQTY(_lqtyAmount);
-    }
-
-function governance_depositLQTYViaPermit_clamped(uint256 _lqtyAmount) public asActor {
-        _lqtyAmount %= lqty.balanceOf(_getActor()) + 1;
-        
-        PermitParams memory permitParams = PermitParams({
-            owner: _getActor(),
-            spender: address(governance),
-            value: _lqtyAmount,
-            deadline: block.timestamp + 1000,
-            v: 0,
-            r: bytes32(0),
-            s: bytes32(0)
-        });
-        
-        governance.depositLQTYViaPermit(_lqtyAmount, permitParams);
-    }
-
-    function governance_registerInitiativeWithThreshold_clamped(uint256 _lqtyAmount) public asActor {
-        // Ensure we have enough LQTY to meet registration threshold
-        _lqtyAmount %= lqty.balanceOf(_getActor()) + 1;
-        
-        // Deposit LQTY first to establish voting power
-        vm.prank(_getActor());
-        lqty.transfer(address(governance), _lqtyAmount);
-        
-        // Try to register initiative
-        governance_registerInitiative(address(bribeInitiative));
-    }
-
-    function governance_claimForInitiativeWithDifferentAmounts_clamped() public asActor {
-        // Try claiming for different initiatives
-        address[] memory initiatives = new address[](3);
-        initiatives[0] = address(bribeInitiative);
-        initiatives[1] = address(curveV2GaugeRewards);
-        initiatives[2] = address(uniV4MerklRewards);
-        
-        uint256 randomIndex = block.timestamp % 3;
-        governance_claimForInitiative(initiatives[randomIndex]);
-    }
-
-    function governance_registerInitiative_clamped() public asActor {
-        governance_registerInitiative(address(bribeInitiative));
-    }
-
-    function governance_withdrawLQTY_clamped(uint256 _lqtyAmount) public asActor {
-        (uint256 unallocatedLQTY,,,) = governance.userStates(_getActor());
-        _lqtyAmount %= unallocatedLQTY + 1;
-        
-        governance_withdrawLQTY(_lqtyAmount);
-    }
-
-    function governance_claimForInitiative_clamped() public asActor {
-        governance_claimForInitiative(address(bribeInitiative));
-    }
-
-    function governance_resetAllocations_clamped() public asActor {
-        address[] memory initiativesToReset = new address[](1);
-        initiativesToReset[0] = address(bribeInitiative);
-        
-        governance_resetAllocations(initiativesToReset, false);
-    }
-
-    function governance_calculateVotingThreshold_clamped() public asActor {
-        governance_calculateVotingThreshold();
-    }
-
-    function governance_claimFromStakingV1_clamped() public asActor {
-        governance_claimFromStakingV1(_getActor());
-    }
-
-    function governance_deployUserProxy_clamped() public asActor {
-        governance_deployUserProxy();
-    }
-
-    function governance_getInitiativeState_clamped() public asActor {
-        governance_getInitiativeState(address(bribeInitiative));
-    }
-
-    function governance_multiDelegateCall_clamped() public asActor {
-        bytes[] memory calls = new bytes[](1);
-        calls[0] = abi.encodeWithSignature("depositLQTY(uint256)", uint256(0));
-        
-        governance_multiDelegateCall(calls);
-    }
-
-    function governance_getLatestVotingThreshold_clamped() public asActor {
-        governance_getLatestVotingThreshold();
-    }
-
-    function governance_calculateVotingThresholdWithVotes_clamped(uint256 _votes) public asActor {
-        _votes %= 1000000e18 + 1; // Reasonable vote range
-        governance.calculateVotingThreshold(_votes);
-    }
-
-    function governance_getVotesSnapshot_clamped() public asActor {
-        governance.votesSnapshot();
-    }
-
-    function governance_getVotesForInitiativeSnapshot_clamped() public asActor {
-        governance.votesForInitiativeSnapshot(address(bribeInitiative));
-    }
-
-    function governance_getGlobalState_clamped() public asActor {
-        governance.globalState();
-    }
-
-    function governance_getRegisteredInitiatives_clamped() public asActor {
-        governance.registeredInitiatives(address(bribeInitiative));
-    }
-
-    function governance_getLQTYAllocatedByUserToInitiative_clamped() public asActor {
-        address user = _getActor();
-        governance.lqtyAllocatedByUserToInitiative(user, address(bribeInitiative));
-    }
-
-    function governance_registerInitialInitiatives_clamped() public asActor {
-        address[] memory initiatives = new address[](1);
-        initiatives[0] = address(bribeInitiative);
-        
-        governance_registerInitialInitiatives(initiatives);
-    }
-
-    function governance_snapshotVotesForInitiative_clamped() public asActor {
-        governance_snapshotVotesForInitiative(address(bribeInitiative));
-    }
-
-    function governance_unregisterInitiative_clamped() public asActor {
-        governance_unregisterInitiative(address(bribeInitiative));
-    }
-
     /// AUTO GENERATED TARGET FUNCTIONS - WARNING: DO NOT DELETE OR MODIFY THIS LINE ///
 
     function governance_allocateLQTY(
@@ -272,11 +88,15 @@ function governance_depositLQTYViaPermit_clamped(uint256 _lqtyAmount) public asA
         governance.getInitiativeState(_initiative);
     }
 
-    function governance_multiDelegateCall(bytes[] memory _calls) public asActor {
+    function governance_multiDelegateCall(
+        bytes[] memory _calls
+    ) public asActor {
         governance.multiDelegateCall(_calls);
     }
 
-    function governance_registerInitialInitiatives(address[] memory _initiatives) public asActor {
+    function governance_registerInitialInitiatives(
+        address[] memory _initiatives
+    ) public asActor {
         governance.registerInitialInitiatives(_initiatives);
     }
 
