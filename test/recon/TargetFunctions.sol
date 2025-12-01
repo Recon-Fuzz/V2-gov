75d4b1e2fca7313b91d90fc33d40ef1c9db1dd50
nican0r <94120714+nican0r@users.noreply.github.com>
2025-12-01 14:03:06 +0800

    chore: remove latest artifacts

--- test/recon/TargetFunctions.sol
+++ test/recon/TargetFunctions.sol
@@ -15,7 +15,9 @@ import {PermitParams} from "src/utils/Types.sol";
 // NOTE: Always import and apply them in alphabetical order, so much easier to debug!
 import {AdminTargets} from "./targets/AdminTargets.sol";
 import {BribeInitiativeTargets} from "./targets/BribeInitiativeTargets.sol";
-import {CurveV2GaugeRewardsTargets} from "./targets/CurveV2GaugeRewardsTargets.sol";
+import {
+    CurveV2GaugeRewardsTargets
+} from "./targets/CurveV2GaugeRewardsTargets.sol";
 import {DoomsdayTargets} from "./targets/DoomsdayTargets.sol";
 import {GovernanceTargets} from "./targets/GovernanceTargets.sol";
 import {ManagersTargets} from "./targets/ManagersTargets.sol";
@@ -31,122 +33,5 @@ abstract contract TargetFunctions is
     UniV4MerklRewardsTargets
 {
     /// CUSTOM TARGET FUNCTIONS - Add your own target functions here ///
-    
-    function shortcut_allocateLQTY(uint256 _lqtyAmount, uint256 _voteAmount, uint256 _vetoAmount) public {
-        // Switch to actor 0 and deposit LQTY to have voting power
-        switchActor(0);
-        governance_depositLQTY_clamped(_lqtyAmount);
-        
-        // Register the bribe initiative with actor 0
-        governance_registerInitiative_clamped();
-        
-        // Allocate LQTY to the bribe initiative
-        governance_allocateLQTY_clamped(_voteAmount, _vetoAmount);
-    }
-    
-    function shortcut_claimForInitiative(uint256 _lqtyAmount, uint256 _voteAmount) public {
-        // Switch to actor 0 and set up the initiative
-        switchActor(0);
-        governance_depositLQTY_clamped(_lqtyAmount);
-        governance_registerInitiative_clamped();
-        governance_allocateLQTY_clamped(_voteAmount, 0);
-        
-        // Fast forward time to complete the epoch and make claimable
-        vm.warp(block.timestamp + 7 days + 1 hours);
-        
-        // Claim for the initiative
-        governance_claimForInitiative_clamped();
-    }
-    
-    function shortcut_claimBribes(uint256 _lqtyAmount, uint256 _voteAmount, uint256 _boldAmount, uint256 _bribeTokenAmount) public {
-        // Switch to actor 0 and set up the initiative with voting power
-        switchActor(0);
-        governance_depositLQTY_clamped(_lqtyAmount);
-        governance_registerInitiative_clamped();
-        governance_allocateLQTY_clamped(_voteAmount, 0);
-        
-        // Switch to actor 1 to deposit bribes
-        switchActor(1);
-        bribeInitiative_depositBribe_clamped(_boldAmount, _bribeTokenAmount);
-        
-        // Fast forward time to complete the epoch
-        vm.warp(block.timestamp + 7 days + 1 hours);
-        
-        // Claim bribes with actor 0 (the voter)
-        switchActor(0);
-        bribeInitiative_claimBribes_clamped();
-    }
-    
-    function shortcut_registerInitiative(uint256 _lqtyAmount) public {
-        // Switch to actor 0 and deposit LQTY for voting power
-        switchActor(0);
-        governance_depositLQTY_clamped(_lqtyAmount);
-        
-        // Wait a bit to ensure voting power has age
-        vm.warp(block.timestamp + 1 hours);
-        
-        // Register the initiative
-        governance_registerInitiative_clamped();
-    }
-    
-    function shortcut_unregisterInitiative(uint256 _lqtyAmount, uint256 _voteAmount) public {
-        // Switch to actor 0 and set up an initiative that will become unregisterable
-        switchActor(0);
-        governance_depositLQTY_clamped(_lqtyAmount);
-        governance_registerInitiative_clamped();
-        governance_allocateLQTY_clamped(_voteAmount, 0);
-        
-        // Fast forward multiple epochs to make it unregisterable (after UNREGISTRATION_AFTER_EPOCHS)
-        vm.warp(block.timestamp + (7 days * 5) + 1 hours);
-        
-        // Unregister the initiative
-        governance_unregisterInitiative(address(bribeInitiative));
-    }
-    
-    function shortcut_resetAllocations(uint256 _lqtyAmount, uint256 _voteAmount) public {
-        // Switch to actor 0 and set up allocations to reset
-        switchActor(0);
-        governance_depositLQTY_clamped(_lqtyAmount);
-        governance_registerInitiative_clamped();
-        governance_allocateLQTY_clamped(_voteAmount, 0);
-        
-        // Reset the allocations
-        governance_resetAllocations_clamped();
-    }
-    
-    function shortcut_snapshotVotesForInitiative(uint256 _lqtyAmount, uint256 _voteAmount) public {
-        // Switch to actor 0 and set up voting activity
-        switchActor(0);
-        governance_depositLQTY_clamped(_lqtyAmount);
-        governance_registerInitiative_clamped();
-        governance_allocateLQTY_clamped(_voteAmount, 0);
-        
-        // Take a snapshot of votes for the initiative
-        governance_snapshotVotesForInitiative_clamped();
-    }
-    
-    function shortcut_multiDelegateCall(uint256 _lqtyAmount, uint256 _voteAmount, uint256 _withdrawAmount) public {
-        // Switch to actor 0 and set up governance state
-        switchActor(0);
-        governance_depositLQTY_clamped(_lqtyAmount);
-        governance_registerInitiative_clamped();
-        governance_allocateLQTY_clamped(_voteAmount, 0);
-        
-        // Create a multi-delegate call sequence: deposit more LQTY, then withdraw
-        bytes[] memory calls = new bytes[](2);
-        calls[0] = abi.encodeWithSignature("depositLQTY(uint256)", uint256(100));
-        calls[1] = abi.encodeWithSignature("withdrawLQTY(uint256)", _withdrawAmount);
-        
-        governance_multiDelegateCall(calls);
-    }
-    
-    function shortcut_claimFromStakingV1(uint256 _lqtyAmount) public {
-        // Switch to actor 0 and deposit LQTY to establish a connection to staking V1
-        switchActor(0);
-        governance_depositLQTY_clamped(_lqtyAmount);
-        
-        // Claim from staking V1
-        governance_claimFromStakingV1_clamped();
-    }
     /// AUTO GENERATED TARGET FUNCTIONS - WARNING: DO NOT DELETE OR MODIFY THIS LINE ///
 }
--- test/recon/targets/BribeInitiativeTargets.sol
+++ test/recon/targets/BribeInitiativeTargets.sol
@@ -14,114 +14,7 @@ import {bound} from "../../util/Random.sol";
 import "src/BribeInitiative.sol";
 
 abstract contract BribeInitiativeTargets is BaseTargetFunctions, Properties {
-    /// CUSTOM TARGET FUNCTIONS - Add your own target functions here //
-
-    function bribeInitiative_claimBribes_clamped() public asActor {
-        uint256 mostRecentEpoch = bribeInitiative.getMostRecentUserEpoch(_getActor());
-        IBribeInitiative.ClaimData[] memory claimData = new IBribeInitiative.ClaimData[](1);
-        claimData[0] = IBribeInitiative.ClaimData({
-            epoch: mostRecentEpoch,
-            prevLQTYAllocationEpoch: mostRecentEpoch,
-            prevTotalLQTYAllocationEpoch: mostRecentEpoch
-        });
-        
-        bribeInitiative_claimBribes(claimData);
-    }
-
-    function bribeInitiative_depositBribe_clamped(
-        uint256 _boldAmount,
-        uint256 _bribeTokenAmount
-    ) public asActor {
-        _boldAmount %= bold.balanceOf(_getActor()) + 1;
-        _bribeTokenAmount %= bribeToken.balanceOf(_getActor()) + 1;
-        uint256 currentEpoch = governance.epoch();
-        
-        bribeInitiative_depositBribe(_boldAmount, _bribeTokenAmount, currentEpoch);
-    }
-
-    function bribeInitiative_onAfterAllocateLQTY_clamped() public asActor {
-        uint256 currentEpoch = governance.epoch();
-        address user = _getActor();
-        (uint256 unallocatedLQTY, uint256 unallocatedOffset, uint256 allocatedLQTY, uint256 allocatedOffset) = governance.userStates(user);
-        IGovernance.UserState memory userState = IGovernance.UserState({
-            unallocatedLQTY: unallocatedLQTY,
-            unallocatedOffset: unallocatedOffset,
-            allocatedLQTY: allocatedLQTY,
-            allocatedOffset: allocatedOffset
-        });
-        IGovernance.Allocation memory allocation = IGovernance.Allocation({
-            voteLQTY: 0,
-            voteOffset: 0,
-            vetoLQTY: 0,
-            vetoOffset: 0,
-            atEpoch: currentEpoch
-        });
-        (uint256 voteLQTY, uint256 voteOffset, uint256 vetoLQTY, uint256 vetoOffset, uint256 lastEpochClaim) = governance.initiativeStates(address(bribeInitiative));
-        IGovernance.InitiativeState memory initiativeState = IGovernance.InitiativeState({
-            voteLQTY: voteLQTY,
-            voteOffset: voteOffset,
-            vetoLQTY: vetoLQTY,
-            vetoOffset: vetoOffset,
-            lastEpochClaim: lastEpochClaim
-        });
-        
-        bribeInitiative_onAfterAllocateLQTY(currentEpoch, user, userState, allocation, initiativeState);
-    }
-
-    function bribeInitiative_onClaimForInitiative_clamped() public asActor {
-        uint256 currentEpoch = governance.epoch();
-        uint256 claimAmount = 0;
-        
-        bribeInitiative_onClaimForInitiative(currentEpoch, claimAmount);
-    }
-
-    function bribeInitiative_onRegisterInitiative_clamped() public asActor {
-        uint256 currentEpoch = governance.epoch();
-        
-        bribeInitiative_onRegisterInitiative(currentEpoch);
-    }
-
-    function bribeInitiative_onUnregisterInitiative_clamped() public asActor {
-        uint256 currentEpoch = governance.epoch();
-        
-        bribeInitiative_onUnregisterInitiative(currentEpoch);
-    }
-
-    function bribeInitiative_depositBribeMultipleEpochs_clamped(
-        uint256 _boldAmount,
-        uint256 _bribeTokenAmount,
-        uint256 _epochOffset
-    ) public asActor {
-        _boldAmount %= bold.balanceOf(_getActor()) + 1;
-        _bribeTokenAmount %= bribeToken.balanceOf(_getActor()) + 1;
-        uint256 currentEpoch = governance.epoch();
-        uint256 targetEpoch = (currentEpoch + _epochOffset) % 10; // Keep within reasonable range
-        
-        bribeInitiative_depositBribe(_boldAmount, _bribeTokenAmount, targetEpoch);
-    }
-
-    function bribeInitiative_totalLQTYAllocatedByEpoch_clamped(uint256 _epochOffset) public asActor {
-        uint256 currentEpoch = governance.epoch();
-        uint256 targetEpoch = (currentEpoch + _epochOffset) % 20; // Allow some past and future epochs
-        
-        bribeInitiative_totalLQTYAllocatedByEpoch(targetEpoch);
-    }
-
-    function bribeInitiative_lqtyAllocatedByUserAtEpoch_clamped(uint256 _epochOffset) public asActor {
-        uint256 currentEpoch = governance.epoch();
-        uint256 targetEpoch = (currentEpoch + _epochOffset) % 20; // Allow some past and future epochs
-        address user = _getActor();
-        
-        bribeInitiative_lqtyAllocatedByUserAtEpoch(user, targetEpoch);
-    }
-
-    function bribeInitiative_checkClaimedBribeAtEpoch_clamped(uint256 _epochOffset) public asActor {
-        uint256 currentEpoch = governance.epoch();
-        uint256 targetEpoch = (currentEpoch + _epochOffset) % 20; // Allow some past and future epochs
-        address user = _getActor();
-        
-        bribeInitiative_checkClaimedBribeAtEpoch(user, targetEpoch);
-    }
+    /// CUSTOM TARGET FUNCTIONS - Add your own target functions here ///
 
     /// AUTO GENERATED TARGET FUNCTIONS - WARNING: DO NOT DELETE OR MODIFY THIS LINE ///
 
@@ -170,15 +63,23 @@ abstract contract BribeInitiativeTargets is BaseTargetFunctions, Properties {
         bribeInitiative.onUnregisterInitiative(0);
     }
 
-    function bribeInitiative_totalLQTYAllocatedByEpoch(uint256 _epoch) public asActor {
+    function bribeInitiative_totalLQTYAllocatedByEpoch(
+        uint256 _epoch
+    ) public asActor {
         bribeInitiative.totalLQTYAllocatedByEpoch(_epoch);
     }
 
-    function bribeInitiative_lqtyAllocatedByUserAtEpoch(address _user, uint256 _epoch) public asActor {
+    function bribeInitiative_lqtyAllocatedByUserAtEpoch(
+        address _user,
+        uint256 _epoch
+    ) public asActor {
         bribeInitiative.lqtyAllocatedByUserAtEpoch(_user, _epoch);
     }
 
-    function bribeInitiative_checkClaimedBribeAtEpoch(address _user, uint256 _epoch) public asActor {
+    function bribeInitiative_checkClaimedBribeAtEpoch(
+        address _user,
+        uint256 _epoch
+    ) public asActor {
         bribeInitiative.claimedBribeAtEpoch(_user, _epoch);
     }
 }
