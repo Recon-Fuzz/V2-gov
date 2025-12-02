4e9e9748446973cee87e9111552ad04053dc633a
nican0r <94120714+nican0r@users.noreply.github.com>
2025-12-02 15:02:16 +0800

    Step 6: Phase 2: Creating Clamped Handlers
    Implement clamped function handlers to achieve full coverage on the system setup

--- test/recon/targets/BribeInitiativeTargets.sol
+++ test/recon/targets/BribeInitiativeTargets.sol
@@ -16,6 +16,103 @@ import "src/BribeInitiative.sol";
 abstract contract BribeInitiativeTargets is BaseTargetFunctions, Properties {
     /// CUSTOM TARGET FUNCTIONS - Add your own target functions here ///
 
+    // Clamped handler for claimBribes
+    function bribeInitiative_claimBribes_clamped(
+        uint256 _numClaims,
+        uint256 _epoch1,
+        uint256 _epoch2,
+        uint256 _prevLQTYAlloc1,
+        uint256 _prevTotalLQTYAlloc1,
+        uint256 _prevLQTYAlloc2,
+        uint256 _prevTotalLQTYAlloc2
+    ) public asActor {
+        // Limit number of claims
+        _numClaims = bound(_numClaims, 1, 3);
+        
+        uint256 currentEpoch = governance.epoch();
+        
+        IBribeInitiative.ClaimData[] memory claimData = new IBribeInitiative.ClaimData[](_numClaims);
+        
+        for (uint256 i = 0; i < _numClaims; i++) {
+            if (i == 0) {
+                claimData[i].epoch = bound(_epoch1, 1, currentEpoch > 0 ? currentEpoch - 1 : 0);
+                claimData[i].prevLQTYAllocationEpoch = bound(_prevLQTYAlloc1, 0, claimData[i].epoch);
+                claimData[i].prevTotalLQTYAllocationEpoch = bound(_prevTotalLQTYAlloc1, 0, claimData[i].epoch);
+            } else {
+                claimData[i].epoch = bound(_epoch2, 1, currentEpoch > 0 ? currentEpoch - 1 : 0);
+                claimData[i].prevLQTYAllocationEpoch = bound(_prevLQTYAlloc2, 0, claimData[i].epoch);
+                claimData[i].prevTotalLQTYAllocationEpoch = bound(_prevTotalLQTYAlloc2, 0, claimData[i].epoch);
+            }
+        }
+        
+        bribeInitiative.claimBribes(claimData);
+    }
+
+    // Clamped handler for depositBribe
+    function bribeInitiative_depositBribe_clamped(
+        uint256 _boldAmount,
+        uint256 _bribeTokenAmount,
+        uint256 _epoch
+    ) public asActor {
+        address actor = _getActor();
+        
+        // Bound amounts to reasonable values based on balance
+        uint256 boldBalance = bold.balanceOf(actor);
+        uint256 bribeBalance = bribeToken.balanceOf(actor);
+        
+        _boldAmount = bound(_boldAmount, 0, boldBalance > 0 ? boldBalance / 2 : 0);
+        _bribeTokenAmount = bound(_bribeTokenAmount, 0, bribeBalance > 0 ? bribeBalance / 2 : 0);
+        
+        // Bound epoch to current or future epochs
+        uint256 currentEpoch = governance.epoch();
+        _epoch = bound(_epoch, currentEpoch, currentEpoch + 10);
+        
+        bribeInitiative.depositBribe(_boldAmount, _bribeTokenAmount, _epoch);
+    }
+
+    // Clamped handler for totalLQTYAllocatedByEpoch
+    function bribeInitiative_totalLQTYAllocatedByEpoch_clamped(
+        uint256 _epoch
+    ) public asActor {
+        uint256 currentEpoch = governance.epoch();
+        _epoch = bound(_epoch, 1, currentEpoch + 5);
+        bribeInitiative.totalLQTYAllocatedByEpoch(_epoch);
+    }
+
+    // Clamped handler for lqtyAllocatedByUserAtEpoch
+    function bribeInitiative_lqtyAllocatedByUserAtEpoch_clamped(
+        uint256 _userIndex,
+        uint256 _epoch
+    ) public asActor {
+        address[] memory actors = _getActors();
+        require(actors.length > 0, "No actors");
+        
+        _userIndex = bound(_userIndex, 0, actors.length - 1);
+        address user = actors[_userIndex];
+        
+        uint256 currentEpoch = governance.epoch();
+        _epoch = bound(_epoch, 1, currentEpoch + 5);
+        
+        bribeInitiative.lqtyAllocatedByUserAtEpoch(user, _epoch);
+    }
+
+    // Clamped handler for claimedBribeAtEpoch
+    function bribeInitiative_checkClaimedBribeAtEpoch_clamped(
+        uint256 _userIndex,
+        uint256 _epoch
+    ) public asActor {
+        address[] memory actors = _getActors();
+        require(actors.length > 0, "No actors");
+        
+        _userIndex = bound(_userIndex, 0, actors.length - 1);
+        address user = actors[_userIndex];
+        
+        uint256 currentEpoch = governance.epoch();
+        _epoch = bound(_epoch, 1, currentEpoch + 5);
+        
+        bribeInitiative.claimedBribeAtEpoch(user, _epoch);
+    }
+
     /// AUTO GENERATED TARGET FUNCTIONS - WARNING: DO NOT DELETE OR MODIFY THIS LINE ///
 
     function bribeInitiative_claimBribes(
--- test/recon/targets/GovernanceTargets.sol
+++ test/recon/targets/GovernanceTargets.sol
@@ -17,6 +17,228 @@ import "src/Governance.sol";
 abstract contract GovernanceTargets is BaseTargetFunctions, Properties {
     /// CUSTOM TARGET FUNCTIONS - Add your own target functions here ///
 
+    // Clamped handler for depositLQTY
+    function governance_depositLQTY_clamped(
+        uint256 _lqtyAmount
+    ) public asActor {
+        address actor = _getActor();
+        uint256 balance = lqty.balanceOf(actor);
+        
+        // Bound to reasonable deposit amounts
+        _lqtyAmount = bound(_lqtyAmount, 1e18, balance > 0 ? balance / 2 : 1e18);
+        
+        governance.depositLQTY(_lqtyAmount);
+    }
+
+    // Clamped handler for withdrawLQTY
+    function governance_withdrawLQTY_clamped(
+        uint256 _lqtyAmount
+    ) public asActor {
+        address actor = _getActor();
+        (uint256 unallocated,,,) = governance.userStates(actor);
+        
+        if (unallocated == 0) return;
+        
+        // Bound to available unallocated LQTY
+        _lqtyAmount = bound(_lqtyAmount, 1e18, unallocated);
+        
+        governance.withdrawLQTY(_lqtyAmount);
+    }
+
+    // Clamped handler for allocateLQTY
+    function governance_allocateLQTY_clamped(
+        uint256 _numInitiatives,
+        uint256 _voteAmount1,
+        uint256 _voteAmount2,
+        bool _isVeto1,
+        bool _isVeto2
+    ) public asActor {
+        address actor = _getActor();
+        (uint256 unallocated,,,) = governance.userStates(actor);
+        
+        if (unallocated == 0) return;
+        
+        // Limit number of initiatives
+        _numInitiatives = bound(_numInitiatives, 1, 2);
+        
+        address[] memory initiativesToReset = new address[](0);
+        address[] memory initiatives = new address[](_numInitiatives);
+        int256[] memory absoluteLQTYVotes = new int256[](_numInitiatives);
+        int256[] memory absoluteLQTYVetos = new int256[](_numInitiatives);
+        
+        // Use bribeInitiative as one initiative
+        initiatives[0] = address(bribeInitiative);
+        
+        if (_numInitiatives > 1) {
+            // Use curveV2GaugeRewards as second initiative
+            initiatives[1] = address(curveV2GaugeRewards);
+        }
+        
+        // Distribute unallocated LQTY between initiatives
+        uint256 amount1 = bound(_voteAmount1, 0, unallocated);
+        uint256 amount2 = 0;
+        
+        if (_numInitiatives > 1 && unallocated > amount1) {
+            amount2 = bound(_voteAmount2, 0, unallocated - amount1);
+        }
+        
+        if (_isVeto1) {
+            absoluteLQTYVotes[0] = 0;
+            absoluteLQTYVetos[0] = int256(amount1);
+        } else {
+            absoluteLQTYVotes[0] = int256(amount1);
+            absoluteLQTYVetos[0] = 0;
+        }
+        
+        if (_numInitiatives > 1) {
+            if (_isVeto2) {
+                absoluteLQTYVotes[1] = 0;
+                absoluteLQTYVetos[1] = int256(amount2);
+            } else {
+                absoluteLQTYVotes[1] = int256(amount2);
+                absoluteLQTYVetos[1] = 0;
+            }
+        }
+        
+        governance.allocateLQTY(initiativesToReset, initiatives, absoluteLQTYVotes, absoluteLQTYVetos);
+    }
+
+    // Clamped handler for resetAllocations
+    function governance_resetAllocations_clamped(
+        uint256 _numToReset,
+        bool _checkAll
+    ) public asActor {
+        address actor = _getActor();
+        (,, uint256 allocated,) = governance.userStates(actor);
+        
+        if (allocated == 0) return;
+        
+        // Limit number of initiatives to reset
+        _numToReset = bound(_numToReset, 1, 2);
+        
+        address[] memory initiativesToReset = new address[](_numToReset);
+        initiativesToReset[0] = address(bribeInitiative);
+        
+        if (_numToReset > 1) {
+            initiativesToReset[1] = address(curveV2GaugeRewards);
+        }
+        
+        governance.resetAllocations(initiativesToReset, _checkAll);
+    }
+
+    // Clamped handler for registerInitiative  
+    function governance_registerInitiative_clamped(
+        uint256 _initiativeIndex
+    ) public asActor {
+        address initiative;
+        
+        // Cycle through different initiatives
+        if (_initiativeIndex % 3 == 0) {
+            initiative = address(bribeInitiative);
+        } else if (_initiativeIndex % 3 == 1) {
+            initiative = address(curveV2GaugeRewards);
+        } else {
+            initiative = address(uniV4MerklRewards);
+        }
+        
+        // Check if already registered
+        if (governance.registeredInitiatives(initiative) != 0) return;
+        
+        address actor = _getActor();
+        uint256 boldBalance = bold.balanceOf(actor);
+        
+        // Check if actor has enough BOLD for registration fee
+        uint256 regFee = governance.REGISTRATION_FEE();
+        if (boldBalance < regFee) return;
+        
+        governance.registerInitiative(initiative);
+    }
+
+    // Clamped handler for unregisterInitiative
+    function governance_unregisterInitiative_clamped(
+        uint256 _initiativeIndex
+    ) public asActor {
+        address initiative;
+        
+        // Cycle through different initiatives
+        if (_initiativeIndex % 3 == 0) {
+            initiative = address(bribeInitiative);
+        } else if (_initiativeIndex % 3 == 1) {
+            initiative = address(curveV2GaugeRewards);
+        } else {
+            initiative = address(uniV4MerklRewards);
+        }
+        
+        governance.unregisterInitiative(initiative);
+    }
+
+    // Clamped handler for claimForInitiative
+    function governance_claimForInitiative_clamped(
+        uint256 _initiativeIndex
+    ) public asActor {
+        address initiative;
+        
+        // Cycle through different initiatives
+        if (_initiativeIndex % 3 == 0) {
+            initiative = address(bribeInitiative);
+        } else if (_initiativeIndex % 3 == 1) {
+            initiative = address(curveV2GaugeRewards);
+        } else {
+            initiative = address(uniV4MerklRewards);
+        }
+        
+        governance.claimForInitiative(initiative);
+    }
+
+    // Clamped handler for snapshotVotesForInitiative
+    function governance_snapshotVotesForInitiative_clamped(
+        uint256 _initiativeIndex
+    ) public asActor {
+        address initiative;
+        
+        // Cycle through different initiatives
+        if (_initiativeIndex % 3 == 0) {
+            initiative = address(bribeInitiative);
+        } else if (_initiativeIndex % 3 == 1) {
+            initiative = address(curveV2GaugeRewards);
+        } else {
+            initiative = address(uniV4MerklRewards);
+        }
+        
+        governance.snapshotVotesForInitiative(initiative);
+    }
+
+    // Clamped handler for getInitiativeState
+    function governance_getInitiativeState_clamped(
+        uint256 _initiativeIndex
+    ) public asActor {
+        address initiative;
+        
+        // Cycle through different initiatives
+        if (_initiativeIndex % 3 == 0) {
+            initiative = address(bribeInitiative);
+        } else if (_initiativeIndex % 3 == 1) {
+            initiative = address(curveV2GaugeRewards);
+        } else {
+            initiative = address(uniV4MerklRewards);
+        }
+        
+        governance.getInitiativeState(initiative);
+    }
+
+    // Clamped handler for claimFromStakingV1
+    function governance_claimFromStakingV1_clamped(
+        uint256 _recipientIndex
+    ) public asActor {
+        address[] memory actors = _getActors();
+        require(actors.length > 0, "No actors");
+        
+        _recipientIndex = bound(_recipientIndex, 0, actors.length - 1);
+        address recipient = actors[_recipientIndex];
+        
+        governance.claimFromStakingV1(recipient);
+    }
+
     /// AUTO GENERATED TARGET FUNCTIONS - WARNING: DO NOT DELETE OR MODIFY THIS LINE ///
 
     function governance_allocateLQTY(
