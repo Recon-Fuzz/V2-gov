47afd032ed18d39adc6e612de6d78f791651216e
nican0r <94120714+nican0r@users.noreply.github.com>
2025-12-10 15:35:43 +0800

    Step 1: Phase 0a: Analyze Setup Decisions
    Analyze contracts and target functions to determine setup decisions

--- test/recon/Setup.sol
+++ test/recon/Setup.sol
@@ -61,7 +61,8 @@ abstract contract Setup is BaseSetup, ActorManager, AssetManager, Utils {
     address internal userProxy;
 
     // Private key for permit testing
-    uint256 internal userPrivateKey = 23868421370328131711506074113045611601786642648093516849953535378706721142721;
+    uint256 internal userPrivateKey =
+        23868421370328131711506074113045611601786642648093516849953535378706721142721;
 
     // Struct parameters (single mode)
     PermitParams internal permitParams;
@@ -72,153 +73,18 @@ abstract contract Setup is BaseSetup, ActorManager, AssetManager, Utils {
 
     /// === Setup === ///
     /// This contains all calls to be performed in the tester constructor, both for Echidna and Foundry
-    function setup() internal virtual override {
-        // 1. Time warp - set to epoch start
-        vm.warp(START_TIME);
-
-        // 2. Add actors (address(this) is already an actor by default)
-        // Add the actor with known private key for permit testing
-        _addActor(0x537C8f3d3E18dF5517a58B3fB9D9143697996802);
-        // Add an additional actor for testing
-        _addActor(address(0x1234));
-
-        // 3. Deploy tokens using custom mocks
-        lqty = new MockERC20Tester("Liquity", "LQTY");
-        lusd = new MockERC20Tester("LUSD Stablecoin", "LUSD");
-        bold = new MockERC20Tester("BOLD Stablecoin", "BOLD");
-        bribeToken = new MockERC20Tester("Bribe Token", "BRIBE");
-
-        // Add tokens to asset manager for tracking
-        _addAsset(address(lqty));
-        _addAsset(address(lusd));
-        _addAsset(address(bold));
-        _addAsset(address(bribeToken));
-
-        // 4. Deploy mocks (non-token dependencies)
-        stakingV1 = new MockStakingV1(lqty, lusd);
-        mockLiquidityGauge = new MockLiquidityGauge(address(bold));
-        mockDistributionCreator = new MockDistributionCreator();
-
-        // 5. Deploy core contracts
-        // Configure governance
-        IGovernance.Configuration memory config = IGovernance.Configuration({
-            registrationFee: REGISTRATION_FEE,
-            registrationThresholdFactor: 0.01 ether,
-            unregistrationThresholdFactor: 4 ether,
-            unregistrationAfterEpochs: 4,
-            votingThresholdFactor: 0.04 ether,
-            minClaim: 0,
-            minAccrual: 0,
-            epochStart: START_TIME - EPOCH_DURATION,
-            epochDuration: EPOCH_DURATION,
-            epochVotingCutoff: EPOCH_DURATION - 1 days
-        });
-
-        // Deploy governance with empty initial initiatives
-        address[] memory initialInitiatives = new address[](0);
-        governance = new Governance(
-            address(lqty),
-            address(lusd),
-            address(stakingV1),
-            address(bold),
-            config,
-            address(this),
-            initialInitiatives
-        );
-
-        // Deploy reward contracts
-        curveV2GaugeRewards = new CurveV2GaugeRewards(
-            address(governance),
-            address(bold),
-            address(bribeToken),
-            address(mockLiquidityGauge),
-            7 days
-        );
-
-        uniV4MerklRewards = new MockUniV4MerklRewards(
-            address(governance),
-            address(bold),
-            1e18, // _campaignBoldAmountThreshold
-            bytes32(0), // _uniV4PoolId
-            3333, // _weightFees
-            3333, // _weightToken0
-            3334, // _weightToken1
-            address(mockDistributionCreator)
-        );
-
-        // 6. Post-deploy actions
-        // Deploy initial BribeInitiative instance
-        bribeInitiative = new BribeInitiative(
-            address(governance),
-            address(bold),
-            address(bribeToken)
-        );
-        deployedBribeInitiatives.push(address(bribeInitiative));
-
-        // Warp to epoch 3 (registration is only enabled after epoch 2)
-        // Each epoch is EPOCH_DURATION (7 days), and governance starts at START_TIME - EPOCH_DURATION
-        vm.warp(START_TIME + EPOCH_DURATION * 2 + 1);
-
-        // Register the initial bribe initiative
-        bold.mint(address(this), REGISTRATION_FEE);
-        bold.approve(address(governance), REGISTRATION_FEE);
-        governance.registerInitiative(address(bribeInitiative));
-
-        // 7. Manually mint tokens to all actors and deploy user proxies
-        address[] memory actors = _getActors();
-        address[] memory assets = _getAssets();
-        uint256 mintAmount = type(uint88).max;
-        
-        // Deploy user proxies for the first 2 actors only (leave actor 2 for testing deployUserProxy)
-        for (uint256 i = 0; i < 2 && i < actors.length; i++) {
-            vm.prank(actors[i]);
-            address actorUserProxy = governance.deployUserProxy();
-            
-            // Approve the user proxy to spend LQTY for this actor
-            vm.prank(actors[i]);
-            lqty.approve(actorUserProxy, type(uint256).max);
-            
-            // Approve stakingV1 to spend LQTY from the user proxy
-            vm.prank(actorUserProxy);
-            lqty.approve(address(stakingV1), type(uint256).max);
-            
-            // Store the first user proxy (for address(this))
-            if (i == 0) {
-                userProxy = actorUserProxy;
-            }
-        }
-        
-        for (uint256 i = 0; i < assets.length; i++) {
-            for (uint256 j = 0; j < actors.length; j++) {
-                MockERC20Tester(assets[i]).mint(actors[j], mintAmount);
-            }
-        }
-
-        // 8. Set up approvals for all actors
-        address[] memory approvalArray = new address[](5);
-        approvalArray[0] = address(governance);
-        approvalArray[1] = address(curveV2GaugeRewards);
-        approvalArray[2] = address(uniV4MerklRewards);
-        approvalArray[3] = address(bribeInitiative);
-        approvalArray[4] = userProxy;
-
-        for (uint256 i = 0; i < assets.length; i++) {
-            for (uint256 j = 0; j < actors.length; j++) {
-                for (uint256 k = 0; k < approvalArray.length; k++) {
-                    vm.prank(actors[j]);
-                    MockERC20Tester(assets[i]).approve(approvalArray[k], type(uint256).max);
-                }
-            }
-        }
-    }
+    function setup() internal virtual override {}
 
     /// === Helper Functions === ///
 
     /// @notice Get a deployed BribeInitiative by index
     /// @param index Index to select from deployedBribeInitiatives array
     /// @return address of the selected BribeInitiative
-    function _getDeployedBribeInitiative(uint8 index) internal view returns (address) {
-        return deployedBribeInitiatives[index % deployedBribeInitiatives.length];
+    function _getDeployedBribeInitiative(
+        uint8 index
+    ) internal view returns (address) {
+        return
+            deployedBribeInitiatives[index % deployedBribeInitiatives.length];
     }
 
     /// @notice Get the current epoch number
@@ -233,23 +99,39 @@ abstract contract Setup is BaseSetup, ActorManager, AssetManager, Utils {
     /// @param value Amount of tokens to permit
     /// @param deadline Permit expiration timestamp
     /// @return Valid PermitParams struct with signature
-    function _getValidPermitParams(address owner, address spender, uint256 value, uint256 deadline)
-        internal
-        returns (PermitParams memory)
-    {
+    function _getValidPermitParams(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline
+    ) internal returns (PermitParams memory) {
         // Create the permit digest
-        bytes32 permitTypeHash =
-            keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+        bytes32 permitTypeHash = keccak256(
+            "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
+        );
         bytes32 domainSeparator = lqty.DOMAIN_SEPARATOR();
         uint256 nonce = lqty.nonces(owner);
 
-        bytes32 structHash = keccak256(abi.encode(permitTypeHash, owner, spender, value, nonce, deadline));
-        bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
+        bytes32 structHash = keccak256(
+            abi.encode(permitTypeHash, owner, spender, value, nonce, deadline)
+        );
+        bytes32 digest = keccak256(
+            abi.encodePacked("\x19\x01", domainSeparator, structHash)
+        );
 
         // Sign with the known private key
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivateKey, digest);
 
-        return PermitParams({owner: owner, spender: spender, value: value, deadline: deadline, v: v, r: r, s: s});
+        return
+            PermitParams({
+                owner: owner,
+                spender: spender,
+                value: value,
+                deadline: deadline,
+                v: v,
+                r: r,
+                s: s
+            });
     }
 
     /// === MODIFIERS === ///
